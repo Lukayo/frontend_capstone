89874f858143f164a66bcc90d56485201a8d5ded
lukayo <arronbagamasbad@gmail.com>
2023-10-20 02:05:43 +0800

    re-stuctured

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/Nav'
-import Footer from './components/Footer'
-import Hero from "./components/Hero";
-import Highlights from './components/Highlights';
-import Rating from './components/Rating';
-import About from './components/About';
+import Nav from "./components/Nav"
+import Main from "./components/Main"
+import Menu from './components/Menu';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-          <Nav/>
-          <Hero/>
-          <article><Highlights/></article>
-          <article><Rating/></article>
-          <article><About/></article>
-        <footer>
-          <Footer/>
-        </footer>
+      <Nav/>
+      <Main />
+      <Menu/>
+      <Footer/>
     </>
   );
 }
